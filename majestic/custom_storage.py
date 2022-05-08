417593c8df3018d912d4eccb7528dc77dfb4c3e1
frankjlin16 <flin30@asu.edu>
2022-05-08 05:03:01 +0000

    Modify custom static storage

--- majestic/custom_storage.py
+++ majestic/custom_storage.py
@@ -1,7 +1,5 @@
 from storages.backends.azure_storage import AzureStorage
 
-class PublicAzureStorage(AzureStorage):
-    account_name = 'myaccount'
-    account_key = 'mykey'
-    azure_container = 'mypublic_container'
-    expiration_secs = None
+class StaticAzureStorage(AzureStorage):
+    azure_container = 'static'
+    azure_location = 'static'
