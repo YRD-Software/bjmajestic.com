eacbf5334c8bdcad3baa20817b566f342885a18e
frankjlin16 <flin30@asu.edu>
2022-05-12 20:57:28 -0700

    Comment out

--- majestic/custom_storage.py
+++ majestic/custom_storage.py
@@ -1,5 +1,5 @@
-from storages.backends.azure_storage import AzureStorage
+# from storages.backends.azure_storage import AzureStorage
 
-class StaticAzureStorage(AzureStorage): # FIXME: not working
-    azure_container = 'static'
-    azure_location = 'static'
+# class StaticAzureStorage(AzureStorage): # FIXME: not working
+#     azure_container = 'static'
+#     azure_location = 'static'
