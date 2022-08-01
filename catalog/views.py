ce73391a7f06d71b37a69997c54e51466b8e93a5
frankjlin16 <flin30@asu.edu>
2022-08-01 08:35:39 -0700

    Update view return context

--- catalog/views.py
+++ catalog/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def catalog(request):
     """Display catalog page."""
-    return render(request, 'catalog/catalog.html')
+    context = {'page': 'catalog'}
+    return render(request, 'catalog/catalog.html', context)
