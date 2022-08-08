f379d5050bdb64ccab398f419456669f3f6163ae
frankjlin16 <flin30@asu.edu>
2022-08-08 16:39:04 -0700

    Create model for catalog

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+
+def validate_file_extension(value):
+    """validate_file_extension validates the file extension of the uploaded file
+
+    Args:
+        value: represents the uploaded file
+
+    Raises:
+        ValidationError: is the file extension is not .pdf or .jpg, raises a validation error
+    """
+    if not value.endwith(['.pdf', '.jpg']):
+        raise ValidationError(u'Unsupported file extension.')
+
+
+class Catalog(models.Model):
+    """Catalog defines a catalog file for a given year.
+
+    Args:
+        models (models): provide fields for the model.
+    """
+    year = models.SmallIntegerField(unique=True)
+    file = models.FileField(upload_to='catalogs/%Y/',
+                            validators=[validate_file_extension])
