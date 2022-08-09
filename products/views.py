18df63ba92ad029bde4d2adef64f537242cbbc97
frankjlin16 <flin30@asu.edu>
2022-08-09 14:10:34 -0700

    Add similar products to context

--- products/views.py
+++ products/views.py
@@ -28,4 +28,10 @@ class DetailProductView(generic.DetailView):
 	model = Product
 	template_name = 'homepage/product_detail.html'
 	context_object_name = 'product'
+ 
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["similar_products"] = [1,2,3] 
+		return context
+	
 
