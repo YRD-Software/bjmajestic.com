ad48bfeed747924af7a73f3ee506b8f1b49be6ca
frankjlin16 <flin30@asu.edu>
2022-08-08 17:05:14 -0700

    Modify admin page for catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Catalog
 
 # Register your models here.
+class CatalogAdmin(admin.ModelAdmin):
+    list_display = ('year',)
+
+admin.site.register(Catalog, CatalogAdmin)
