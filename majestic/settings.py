f9e1cc86e653a5af284709be5c0347911e845680
frankjlin16 <flin30@asu.edu>
2022-08-10 22:06:15 -0700

    Modify dev settings

--- majestic/settings.py
+++ majestic/settings.py
@@ -30,10 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-if DEBUG:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-else:
-    ALLOWED_HOSTS = ['majestic.azurewebsites.net']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://majestic.azurewebsites.net']
 
