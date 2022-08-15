316a54c0834b003e3c89181924c275cc53808ca2
frankjlin16 <flin30@asu.edu>
2022-08-14 18:56:30 -0700

    Remove azure backend storage

--- majestic/custom_storage.py
+++ majestic/custom_storage.py
@@ -1,23 +1,4 @@
-from storages.backends.azure_storage import AzureStorage
 from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class StaticAzureStorage(AzureStorage):
-    """StaticAzureStorage: Configure Azure storage for static files.
-
-    Args:
-        AzureStorage (Storage): Azure's storage class.
-    """
-    azure_container = 'static'
-
-
-class MediaAzureStorage(AzureStorage):
-    """MediaAzureStorage: Configure Azure storage for media files.
-
-    Args:
-        AzureStorage (Storage): Azure's storage class.
-    """
-    azure_container = 'media'
 
 
 class StaticS3Storage(S3Boto3Storage):
