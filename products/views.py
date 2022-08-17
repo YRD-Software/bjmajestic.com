32190c7f9e701d652792fb38035bdcc80a7124b3
frankjlin16 <flin30@asu.edu>
2022-08-16 21:14:19 -0700

    Update products list view

--- products/views.py
+++ products/views.py
@@ -1,37 +1,36 @@
 from django.views import generic
 from django.shortcuts import render
-from .models import Product
+from .models import Category, Product
 
 # Create your views here.
 
 
 def index(request):
-	return render(request, 'homepage/index.html', {'page': 'home'})
+    return render(request, 'homepage/index.html', {'page': 'home'})
 
 
 class ProductsView(generic.ListView):
-	"""A list of all the products."""
-	template_name = 'homepage/products.html'
+    """A list of all the products."""
+    template_name = 'homepage/products.html'
+    model = Product
 
-	# Return all products
-	def get_queryset(self):
-		return Product.objects.all()
+    # Define context to be passed to template
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = {'page': 'product', 'products': self.get_queryset(
+        ), 'categories': self.get_categories()}
+        return context
 
-	# Define context to be passed to template
-	def get_context_data(self, *, object_list=None, **kwargs):
-		context = {'page': 'product', 'products': self.get_queryset()}
-		return context
+    def get_categories(self):
+        return Category.objects.all()
 
 
 class DetailProductView(generic.DetailView):
-	"""The view for displaying details about a product."""
-	model = Product
-	template_name = 'homepage/product_detail.html'
-	context_object_name = 'product'
- 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		context["similar_products"] = [1,2,3] 
-		return context
-	
+    """The view for displaying details about a product."""
+    model = Product
+    template_name = 'homepage/product_detail.html'
+    context_object_name = 'product'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["similar_products"] = [1, 2, 3]
+        return context
