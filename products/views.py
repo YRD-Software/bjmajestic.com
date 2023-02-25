dcca0cf712bec2f73655a7a1e3eab9f1b3c7af7c
Frankie <67761168+frankjlin16@users.noreply.github.com>
2023-02-25 15:32:51 -0700

    Update project list view

--- products/views.py
+++ products/views.py
@@ -6,32 +6,29 @@ from .models import Product
 
 
 def index(request):
-	return render(request, 'homepage/index.html', {'page': 'home'})
+    return render(request, 'homepage/index.html', {'page': 'home'})
 
 
 class ProductsView(generic.ListView):
-	"""A list of all the products."""
-	template_name = 'homepage/products.html'
+    """A list of all the products."""
+    template_name = 'homepage/products.html'
+    model = Product
+    context_object_name = 'products'
 
-	# Return all products
-	def get_queryset(self):
-		return Product.objects.all()
-
-	# Define context to be passed to template
-	def get_context_data(self, *, object_list=None, **kwargs):
-		context = {'page': 'product', 'products': self.get_queryset()}
-		return context
+    # Define context to be passed to template
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page'] = 'product'
+        return context
 
 
 class DetailProductView(generic.DetailView):
-	"""The view for displaying details about a product."""
-	model = Product
-	template_name = 'homepage/product_detail.html'
-	context_object_name = 'product'
- 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		context["similar_products"] = [1,2,3] 
-		return context
-	
+    """The view for displaying details about a product."""
+    model = Product
+    template_name = 'homepage/product_detail.html'
+    context_object_name = 'product'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["similar_products"] = [1, 2, 3]
+        return context
