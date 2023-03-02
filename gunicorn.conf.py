ec41178f0a02489ad07282def00b418ffe2f6d0c
Frankie <67761168+frankjlin16@users.noreply.github.com>
2023-03-01 23:49:54 -0700

    Update gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,20 +1,19 @@
 """Gunicorn config file"""
 
+import multiprocessing
+
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
 wsgi_app = "majestic.wsgi:application"
-# The granularity of Error log outputs
-loglevel = "debug"
 # The number of worker processes for handling requests
-workers = 2
+workers = multiprocessing.cpu_count() * 2 + 1
 # The socket to bind
 bind = "0.0.0.0:8000"
-# Restart workers when code changes (development only!)
-reload = True
 # Write access and error info to /var/log
-accesslog = errorlog = "/var/log/gunicorn/dev.log"
+accesslog = "/var/log/gunicorn/access.log"
+errorlog = "/var/log/gunicorn/error.log"
 # Redirect stdout/stderr to log file
 capture_output = True
 # PID file so you can easily fetch process ID
-pidfile = "/var/run/gunicorn/dev.pid"
+pidfile = "/var/run/gunicorn/prod.pid"
 # Daemonize the Gunicorn process (detach & enter background)
 daemon = True
