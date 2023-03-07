a02186d90b41e9c587a8cc41db237bffe4b15161
Frankie <67761168+frankjlin16@users.noreply.github.com>
2023-03-06 23:13:47 -0700

    Update admin page with category model

--- products/admin.py
+++ products/admin.py
@@ -1,34 +1,46 @@
 """ Admin configuration for products app. """
 from django.contrib import admin
 
-from .models import Product, ProductPhoto
+from .models import Category, Product, ProductPhoto
 
 
 class DetailPhotosInline(admin.TabularInline):
-    """Allow admin to edit ProductPhoto objects through Product admin page."""
+    """Inline form for adding extra photos to a product."""
+
     model = ProductPhoto
     extra = 2
 
 
 class ProductAdmin(admin.ModelAdmin):
     """Define how the Products page in admin is going to look."""
+
     fieldsets = [
-        (None, {'fields': ['name']}),
-        ('Details', {'fields': ['description', ]}),
-        ('Main Image', {'fields': ['photo']}),
+        (None, {"fields": ["name"]}),
+        ("Details", {"fields": ["description", "categories"]}),
+        ("Main Image", {"fields": ["photo"]}),
     ]
+    filter_horizontal = ("categories",)
     inlines = [DetailPhotosInline]
-    list_display = ('name',)
-    search_fields = ['name']
+    list_display = ("name",)
+    search_fields = ["name"]
 
 
 class ProductPhotoAdmin(admin.ModelAdmin):
     """Define how the Product Photos page in admin is going to look."""
-    list_display = ('product', 'detail_photo')
-    list_filter = ['product']
+
+    list_display = ("product", "detail_photo")
+    list_filter = ["product"]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Define how the Categories page in admin is going to look."""
+
+    list_display = ("category",)
+    search_fields = ["category"]
 
 
 # Register your models here.
 
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductPhoto, ProductPhotoAdmin)
