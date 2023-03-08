c3a769cd2e966a19091c1387d0fa2b3494ed8b3b
Frankie <67761168+frankjlin16@users.noreply.github.com>
2023-03-08 16:56:42 -0700

    feat: generate random filename for uploaded photos

--- products/models.py
+++ products/models.py
@@ -1,12 +1,21 @@
 """ This module contains the models for the products app."""
+import uuid
 from django.db import models
 from django.urls import reverse
+
+
+def unique_file_path(instance, filename):
+    # Generate a unique file name
+    ext = filename.split(".")[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    return '/'.join([instance.__class__.__name__.lower(), filename])
 
 
 # Create your models here.
 class Category(models.Model):
     """A category for a product"""
-    category = models.CharField('category', max_length=30)
+
+    category = models.CharField("category", max_length=30)
 
     def __str__(self):
         """Return a string rep. of the model"""
@@ -14,12 +23,14 @@ class Category(models.Model):
 
     class Meta:
         """Meta class for Category."""
-        verbose_name_plural = 'Categories'
+
+        verbose_name_plural = "Categories"
 
 
 class Product(models.Model):
     """A description for a product"""
-    name = models.CharField('product', max_length=30)
+
+    name = models.CharField("product", max_length=30)
     description = models.TextField()
     categories = models.ManyToManyField(Category, blank=True)
 
@@ -29,13 +40,14 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         """Return a url to access a detail record for this product."""
-        return reverse('products:products')
+        return reverse("products:products")
 
 
 class ProductPhoto(models.Model):
     """Extra detailed photos of the product."""
+
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    photo = models.ImageField(upload_to=unique_file_path, blank=True, null=True)
 
     def __str__(self):
         """Return string rep for ProductPhoto."""
@@ -43,4 +55,5 @@ class ProductPhoto(models.Model):
 
     class Meta:
         """Meta class for ProductPhoto."""
-        verbose_name_plural = 'Product Photos'
+
+        verbose_name_plural = "Product Photos"
