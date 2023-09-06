aaee76a7522e0ec1a62438535c3a7fc2a9382687
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2023-09-06 15:50:26 -0700

    refactor: update dotenv file name

--- majestic/settings.py
+++ majestic/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+load_dotenv('.env.dev')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
