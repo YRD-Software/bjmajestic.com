faea831b0f2828c617fa99959b724d00ac5a5c86
Frankie J L <67761168+frankjlin16@users.noreply.github.com>
2023-12-12 17:23:40 -0700

    Remove unneed settings

--- majestic/settings.py
+++ majestic/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv('env/.env')
+# load_dotenv('env/.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
