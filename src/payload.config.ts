bf9b126056a8d77dd102f1e98f3acd95e339f941
Frankie L <67761168+frankjlin16@users.noreply.github.com>
2025-03-16 16:55:28 -0700

    feat: integrate Vercel Blob storage client and update media upload configuration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -16,6 +16,7 @@ import { Header } from './Header/config'
 import { plugins } from './plugins'
 import { defaultLexical } from '@/fields/defaultLexical'
 import { getServerSideURL } from './utilities/getURL'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -70,6 +71,13 @@ export default buildConfig({
   plugins: [
     ...plugins,
     // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: true
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    })
   ],
   secret: process.env.PAYLOAD_SECRET,
   sharp,
