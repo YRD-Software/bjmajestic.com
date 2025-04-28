60043d71935d27f033f36b59396e42f47d981cc7
Frankie L <67761168+frankjlin16@users.noreply.github.com>
2025-04-28 15:57:13 -0700

    feat: add dark mode support for text color in RichText component

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -16,7 +16,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
       {/* Content First */}
       <div className="container mb-8 z-10 relative flex items-center justify-center">
         <div className="max-w-[36.5rem] md:text-center">
-          {richText && <RichText className="mb-6 text-background" data={richText} enableGutter={false} />}
+          {richText && <RichText className="mb-6 text-background dark:text-white" data={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
             <ul className="flex md:justify-center gap-4">
               {links.map(({ link }, i) => {
