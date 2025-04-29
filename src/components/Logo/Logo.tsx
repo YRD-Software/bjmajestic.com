c32861cc93786a7171f6636c4a88617dee032484
Frankie L <67761168+frankjlin16@users.noreply.github.com>
2025-04-28 19:17:35 -0700

    feat: replace img with Image component for logo rendering and adjust dimensions

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import React from 'react'
+import Image from 'next/image'
 
 interface Props {
   className?: string
@@ -15,14 +16,13 @@ export const Logo = (props: Props) => {
 
   return (
     /* eslint-disable @next/next/no-img-element */
-    <img
+    <Image
       alt="Beijing Majestic Logo"
-      width={261}
-      height={112}
+      width={96}
+      height={20}
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
       src="/logo-transparent.png"
     />
   )
