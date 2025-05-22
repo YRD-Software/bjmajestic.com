8f2efd99a80d8cbf3c17c5c327e3260aba62ff3a
Frankie L <67761168+frankjlin16@users.noreply.github.com>
2025-05-22 11:04:30 -0700

    feat: implement catalog access permission

--- src/collections/Catalog/index.ts
+++ src/collections/Catalog/index.ts
@@ -1,7 +1,16 @@
 import type { CollectionConfig } from 'payload'
 
+import { anyone } from '@/access/anyone'
+import { authenticated } from '@/access/authenticated'
+
 export const Catalog: CollectionConfig = {
   slug: 'catalog',
+  access: {
+    create: authenticated,
+    delete: authenticated,
+    read: anyone,
+    update: authenticated,
+  },
   fields: [
     {
       name: 'title',
