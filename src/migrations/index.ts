f1230624eca833dee8324f2d9e1236db58e59da0
Frankie L <67761168+frankjlin16@users.noreply.github.com>
2025-05-30 20:31:48 -0700

    staging: add migration file

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250531_032934 from './20250531_032934';
+
+export const migrations = [
+  {
+    up: migration_20250531_032934.up,
+    down: migration_20250531_032934.down,
+    name: '20250531_032934'
+  },
+];
